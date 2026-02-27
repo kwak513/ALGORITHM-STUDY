6ad2c95e22dadc43623904bcb516e2ae5f6b6b69
Chaeyeon Kwak <kwak513@hufs.ac.kr>
2026-02-27 12:22:41 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 28.09 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,42 +3,32 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         
-        // 정답 담을 리스트 생성
-        ArrayList<Integer> answerList = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         
-        // 정답을 새 리스트에 담기
-        for(int i = 0; i < arr.length; i++){
+        list.add(arr[0]);
+        
+        for(int i = 1; i < arr.length; i++){
+            int previousNum = arr[i - 1];
+            int currentNum = arr[i];
             
-            // 첫번째 요소는 무조건 담기
-            if(i == 0){
-                answerList.add(arr[i]);
+            if(previousNum == currentNum){
+                continue;
             }
-            
-            // 두번째 요소부터는 직전 요소와 같지 않을때만 담기
-            else if(i > 0 && arr[i - 1] != arr[i]){
-                answerList.add(arr[i]);
+            else{
+                list.add(currentNum);
             }
         }
         
-        // ArrayList를 int[] 배열로 만드는 과정
-        int answerLength = answerList.size();
-        int[] answer = new int[answerLength];
+        int[] answer = new int[list.size()];
         
-        for(int j = 0; j < answerLength; j++){
-            answer[j] = answerList.get(j);
+        for(int j = 0; j < list.size(); j++){
+            answer[j] = list.get(j);
         }
+        
         
         return answer;
+        
+        
+        
     }
 }
-
-/*
-1. 길이
-- length: 배열 (int[], String[], ...)
-- size(): 컬렉션 (ArrayList, HashMap, HashSet, ...)
-- length(): 문자열 String
-
-2. int[] vs ArrayList<Integer>
-- int[]는 고정 길이
-- 생성 방법: int[] array = new int[길이] / int[] array = {1, 2, 3}
-*/
